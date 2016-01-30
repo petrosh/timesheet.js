90254f78b2c9b6695e925f8353579dffb5add0f2
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2016-01-30 20:16:30 +0100

    First commit

--- script/main.js
+++ script/main.js
@@ -1,25 +1,42 @@
+
 /* global Lib, Timesheet */
 
 (function(){
   'use strict';
-  
+
   Lib.ready(function() {
-    console.log('ads');
-    
-    /* jshint -W031 */
-    new Timesheet('timesheet-default', 2002, 2013, [
-      ['2002', '09/2002', 'A freaking awesome time', 'lorem'],
-      ['06/2002', '09/2003', 'Some great memories', 'ipsum'],
-      ['2003', 'Had very bad luck'],
-      ['10/2003', '2006', 'At least had fun', 'dolor'],
-      ['02/2005', '05/2006', 'Enjoyed those times as well', 'ipsum'],
-      ['07/2005', '09/2005', 'Bad luck again', 'default'],
-      ['10/2005', '2008', 'For a long time nothing happened', 'dolor'],
-      ['01/2008', '05/2009', 'LOST Season #4', 'lorem'],
-      ['01/2009', '05/2009', 'LOST Season #4', 'sit'],
-      ['02/2010', '05/2010', 'LOST Season #5', 'lorem'],
-      ['09/2008', '06/2010', 'FRINGE #1 & #2', 'ipsum']
-    ]);
+
+    function fetchJSONFile(path, callback) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    var data = JSON.parse(httpRequest.responseText);
+                    if (callback) callback(data);
+                }
+            }
+        };
+        httpRequest.open('GET', path);
+        httpRequest.send();
+    }
+
+    fetchJSONFile('timeline.json', function(data){
+      // do something with your data
+      var array = data.timelines.reverse();
+      var timelines = document.getElementById('timelines');
+
+      /* jshint -W031 */
+      for (var i = 0; i < array.length; i++) {
+        var newDiv = document.createElement("div");
+        var title = document.createElement("h2");
+        var newContent = document.createTextNode(array[i].title);
+        title.appendChild(newContent);
+        newDiv.setAttribute("id", "div"+i);
+        timelines.appendChild(title);
+        timelines.appendChild(newDiv);
+        new Timesheet("div"+i, array[i].start, array[i].end, array[i].array);
+      }
+    });
 
     document.querySelector('#switch-dark').addEventListener('click', function() {
       document.querySelector('body').className = 'index black';
@@ -28,5 +45,7 @@
     document.querySelector('#switch-light').addEventListener('click', function() {
       document.querySelector('body').className = 'index white';
     });
-  });
+  }); // end Lib.ready
+
+
 })();
